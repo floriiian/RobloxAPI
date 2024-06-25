8f3cf503bc46a784df37facc53213661de548986
floriiian <florianswzz@gmail.com>
2024-06-25 15:33:45 +0200

    Added Logger & Json Decoding, not working yet for some reason

--- src/main/java/org/robloxapi/Main.java
+++ src/main/java/org/robloxapi/Main.java
@@ -1,4 +1,6 @@
 package org.robloxapi;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import org.apache.logging.log4j.LogManager;
@@ -7,19 +9,30 @@ import okhttp3.Response;
 import java.io.IOException;
 import java.math.BigInteger;
 
+
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
+    // Where the action happens
     public static void main(String[] args) throws IOException {
         // Which game you want to request data for
         BigInteger gameId = new BigInteger("5166944221");
+        ObjectMapper mapper = new ObjectMapper();
+
         try{
             String requestResult = requestData("https://games.roblox.com/v1/games?universeIds=".concat(gameId.toString()));
             logger.debug("Successfully sent request to API");
+
+            JsonNode jsonResult = mapper.readTree(requestResult).get("data");
+            logger.debug(jsonResult.get("rootPlaceId"));
+
+
         }catch(Exception e){
             logger.error("{Failed to send API request: }", e);
         }
     }
 
+    // Sends API Requests
     public static String requestData(String url) throws IOException {
         OkHttpClient client = new OkHttpClient(); // New Client
         Request request = new Request.Builder() // New Request
